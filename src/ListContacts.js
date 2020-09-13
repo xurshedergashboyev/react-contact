3772774967bf09a39c0ec122600f67a8e79f12a6
xurshedergashboyev <62480704+xurshedergashboyev@users.noreply.github.com>
2020-09-13 01:43:14 -0700

    Add files via upload

---
+++ src/ListContacts.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+import escapeRegExp from 'escape-string-regexp'
+import sortBy from 'sort-by'
+
+class ListContacts  extends Component {
+    
+ static propTypes = {
+    contacts: PropTypes.array.isRequired,
+    onDeleteContact:   PropTypes.func.isRequired
+}
+
+
+
+state = {
+    query: ''
+}
+
+updateQuery = (query) => {
+    this.setState({ query: query.trim() })
+}
+
+clearQuery = () => {
+    this.setState({ query: ''})
+}
+    render() {
+
+        const { contacts, onDeleteContact } = this.props
+        const { query } = this.state
+
+        let showingContacts
+        if (query) {
+            const match = new RegExp(escapeRegExp(query), 'i')
+            showingContacts = contacts.filter((contact) =>match.test(contact.name))
+        }
+        else {
+            showingContacts = contacts
+        }
+
+        showingContacts.sort(sortBy('name'))
+
+        return ( 
+            <div className='list-contacts'>
+                {JSON.stringify(this.statenpm )}
+                <div className='list-contacts-top'>
+                    <input 
+                    className='search-contacts'
+                    type='text'
+                    placeholder='Search contacts...' 
+                    value={this.state.query}
+                    onChange={(event) => this.updateQuery(event.target.value)}/>
+
+                    <Link
+                    to='/create'
+                    className='add-contact'
+                    >Add Contact</Link>
+
+                </div>
+
+            {showingContacts.length !== contacts.length && (
+                <div className='showing-contacts'><span>Now Showing {showingContacts.length} of {contacts.length} total</span>
+                <button onClick={this.clearQuery}>Show all</button>
+                </div>
+            )}
+
+            <ol className="contact-list">
+                {showingContacts.map((contact) => (
+                    <li key={contact.id} className='contact-list-item'>
+                        <div className='contact-avatar' style={{
+                            backgroundImage: `url(${contact.avatarURL})`
+                        }} />
+                        <div className='contact-details'>
+                            <p>{contact.name}</p>
+                            <p>{contact.email}</p>
+                        </div>
+                        <button onClick={() => onDeleteContact(contact)} className='contact-remove'>
+                            Remove
+                        </button>
+                    </li>
+                ))}
+            </ol>
+            </div>
+         )
+    }
+}
+
+
+export default ListContacts;
